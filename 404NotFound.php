eb7d2f0fc21493b237e7bc6bec49481f5bfe73bc
nidhalammar <nidhalammar12@gmail.com>
2019-12-02 01:29:37 +0100

    ndcdjbc

--- 404NotFound.php
+++ 404NotFound.php
@@ -124,7 +124,7 @@ body {
 				<h1>4<span>0</span>4</h1>
 			</div>
 			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-			<a href="#">home page</a>
+			<a href="index.html">home page</a>
 		</div>
 	</div>
 
